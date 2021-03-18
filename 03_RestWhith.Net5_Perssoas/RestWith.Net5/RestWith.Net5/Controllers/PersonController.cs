e9ba1726180eb1a8bc0529d547c776570e53c832
Gabriel Coutinho <gcd_94@hotmail.com>
2021-03-18 19:25:50 -0300

    implementações

---
+++ 03_RestWhith.Net5_Perssoas/RestWith.Net5/RestWith.Net5/Controllers/PersonController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWith.Net5.Model;
+using RestWith.Net5.Services.Implementations;
+
+namespace RestWith.Net5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+
+            return Ok(person);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ 03_RestWhith.Net5_Perssoas/RestWith.Net5/RestWith.Net5/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWith.Net5.Model
+{
+    public class Person
+    {
+        public string Address { get; set; }
+        public string FirstName { get; set; }
+        public string Gender { get; set; }
+        public long Id { get; set; }
+        public string LastName { get; set; }
+    }
+}
